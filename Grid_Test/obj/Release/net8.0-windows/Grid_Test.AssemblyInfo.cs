a7589274a9d4685bbc8d609fdad052183bc076e3
bahaa <malkebahaa450>
2024-02-01 05:29:11 +0200

    commit_02

--- Grid_Test/obj/Release/net8.0-windows/Grid_Test.AssemblyInfo.cs
+++ Grid_Test/obj/Release/net8.0-windows/Grid_Test.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Awaad_Project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Grid_Test")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Awaad_Project")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Awaad_Project")]
+[assembly: System.Reflection.AssemblyProductAttribute("Grid_Test")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Grid_Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
